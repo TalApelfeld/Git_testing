a468a0d5b98d19cce114ad86c47dfbe538991ef9
Tal Apelfeld <talapelfeld123@gmail.com>
2024-08-08 11:52:51 +0300

    Added function minus

--- index.ts
+++ index.ts
@@ -2,5 +2,8 @@ function addNumbers(a: number, b: number): number {
   return a + b;
 }
 
+function minusNumbers(a: number, b: number): number {
+  return a - b;
+}
 const result = addNumbers(7, 8);
 console.log(result);
