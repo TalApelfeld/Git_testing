a71b1151c6e07c02c812a916f5d7a6832c8451d1
Tal Apelfeld <talapelfeld123@gmail.com>
2024-08-08 11:46:37 +0300

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function addNumbers(a, b) {
+    return a + b;
+}
+const result = addNumbers(7, 8);
+console.log(result);
---
+++ index.ts
@@ -0,0 +1,6 @@
+function addNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+const result = addNumbers(7, 8);
+console.log(result);
